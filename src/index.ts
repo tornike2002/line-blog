bea1f2bc78c74efccf3093100a8206fa5d5f2327
tornike2002 <toko.toka2000@gmail.com>
2025-03-23 15:42:06 +0400

    configure db

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import { startServer } from "./server";
+import pool from "./config/db";
 
-startServer();
+async function init() {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    console.log("Database Connected:", result.rows[0].now);
+    startServer();
+  } catch (err) {
+    console.error("Error initializing app:", err);
+  }
+}
